5d5b2ece3c7c01cd02ba924b714cc318111ab231
rineheaj <joshtrineheart@gmail.com>
2025-07-20 12:56:06 -0500

    Messing with UI/UX

--- scripts.js
+++ scripts.js
@@ -105,20 +105,79 @@ function toggleSpinner(show) {
 }
 
 
+function createTeamCard(pokemon) {
+    const li = document.createElement("li")
+    li.classList.add("team-card")
+
+    const img = document.createElement("img");
+    img.src = pokemon.img;
+    img.alt = pokemon.name;
+    img.title = "Click to expand for stats";
+    img.addEventListener("click", () => toggleStats(li, pokemon.id));
+
+    const statsDiv = document.createElement("div")
+    statsDiv.classList.add("poke-stats", "hidden");
+    statsDiv.innerHTML = `<p>Loading stats...</p>`;
+
+    li.appendChild(img);
+    li.appendChild(statsDiv);
+    return li;
+}
+
+function toggleStats(cardElement, pokeId) {
+    const statsDiv = cardElement.querySelector(".poke-stats");
+
+    if (cardElement.classList.contains("expanded")) {
+        cardElement.classList.remove("expanded");
+        statsDiv.classList.add("hidden");
+        return;
+    }
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+    .then(res => res.json())
+    .then(pokemon => {
+        statsDiv.innerHTML = formatStatsHTML(pokemon);
+        cardElement.classList.add("expanded");
+        statsDiv.classList.remove("hidden");
+    })
+    .catch(() => {
+        statsDiv.innerHTML = `<p>Failed to load stats.</p>`;
+    })
+}
+
+function formatStatsHTML(pokemon) {
+    const types = pokemon.types.map(t => capitalize(t.type.name)).join(", ");
+    return `
+        <p><strong>Type:</strong> ${types}</p>
+        <p><strong>HP:</strong> ${pokemon.stats[0].base_stat}</p>
+        <p><strong>Attack:</strong> ${pokemon.stats[1].base_stat}</p>
+        <p><strong>Defense:</strong> ${pokemon.stats[2].base_stat}</p>
+        <p><strong>Speed:</strong> ${pokemon.stats[5].base_stat}</p>
+    `;
+}
+
 function renderTeam() {
     teamList.innerHTML = "";
-    team.forEach(member => {
-        const li = document.createElement("li");
-        const img = document.createElement("img");
-        const span = document.createElement("span");
-
-        img.src = member.img;
-        span.textContent = `${member.name} (#${member.id})`;
-
-        li.append(img, span)
-        teamList.append(li)
+    team.forEach(pokemon => {
+        const card = createTeamCard(pokemon);
+        teamList.appendChild(card);
     });
 }
+
+// function renderTeam() {
+//     teamList.innerHTML = "";
+//     team.forEach(member => {
+//         const li = document.createElement("li");
+//         const img = document.createElement("img");
+//         const span = document.createElement("span");
+
+//         img.src = member.img;
+//         span.textContent = `${member.name} (#${member.id})`;
+
+//         li.append(img, span)
+//         teamList.append(li)
+//     });
+// }
 
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);

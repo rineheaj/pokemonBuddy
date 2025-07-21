026c60f83eb5c484d4c5a49039ee3bd0884f26c7
rineheaj <joshtrineheart@gmail.com>
2025-07-20 22:06:32 -0500

    console log stuff

--- api_js_helper.js
+++ api_js_helper.js
@@ -6,6 +6,6 @@ export async function getPokemon(query, signal) {
 
     if (!response.ok) throw new Error(`Pokemon ${query} not found`);
     const data = await response.json();
-    console.log(data);
+    console.log(JSON.stringify(data, null, 4));
     return data;
 }
